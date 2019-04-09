9efb6e89950735c47efbbaf2ae638bd26878b11d
iann <iisackssonoliveira@gmail.com>
2019-04-09 19:57:45 -0300

    Terceira parte - Criando a rota para criação de um registro de uma nova box

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,11 @@
+const Box = require('../models/box');
+
+class BoxController {
+    async store(req, res) {
+        const box = await Box.create(req.body);
+
+        return res.json(box);
+    }
+}
+
+module.exports = new BoxController();
